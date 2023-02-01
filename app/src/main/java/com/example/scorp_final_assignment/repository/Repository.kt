8f160ea0ba10d6455b339ebe7cd8c07fdcfde52f
Urahs <sharumaku@gmail.com>
2023-02-01 15:02:51 +0300

    ...

--- app/src/main/java/com/example/scorp_final_assignment/repository/Repository.kt
+++ app/src/main/java/com/example/scorp_final_assignment/repository/Repository.kt
@@ -4,8 +4,13 @@ import android.Manifest
 
 object Repository {
 
+    const val MinNickNameLength = 4
+    const val AppID = "dca16fbf522b4c6f96ecc88721800310"
+
+    /*
     val permissions = arrayOf(
         Manifest.permission.CAMERA,
         Manifest.permission.RECORD_AUDIO
     )
+     */
 }
--- app/src/main/java/com/example/scorp_final_assignment/ui/HomeFragment.kt
+++ app/src/main/java/com/example/scorp_final_assignment/ui/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,8 +17,11 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.scorp_final_assignment.R
 import com.example.scorp_final_assignment.databinding.FragmentHomeBinding
+import com.example.scorp_final_assignment.repository.Repository.MinNickNameLength
 
 class HomeFragment : Fragment() {
 
@@ -25,11 +30,13 @@ class HomeFragment : Fragment() {
 
     private val requestPermission = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()){ permissions->
+        var permissionCounter = 0
         permissions.forEach{
-            val permissionName = it.key
-            val isGranted = it.value
-            // TODO: is there anything that should I handle here?
+            if(it.value)
+                permissionCounter++
         }
+        if(permissionCounter == permissions.size)
+            findNavController().navigate(R.id.action_homeFragment_to_liveChatFragment)
     }
 
     override fun onCreateView(
@@ -43,12 +50,29 @@ class HomeFragment : Fragment() {
             progressButtonClicked()
         }
 
+        handleNickName()
+
         return binding.root
     }
 
 
+    private fun handleNickName() {
+        val nickNameTextField = binding.nickNameTV
+        binding.progressButton.isEnabled = false
+        nickNameTextField.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(currentText: Editable?) {
+                binding.progressButton.isEnabled = currentText.toString() != "" && currentText.toString().length >= MinNickNameLength
+            }
+        })
+    }
+
+
     private fun progressButtonClicked(){
-        //findNavController().navigate(R.id.action_homeFragment_to_liveChatFragment)
+
         var errorMessage = "You should give the following permission(s) before continue\n\n"
         var permissionsGranted = true
         if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
